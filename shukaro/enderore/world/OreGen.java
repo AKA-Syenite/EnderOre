6bbc051c209618838995cc1afaeb4726c8fb49fe
Ross Swartz <ross.swartz@gmail.com>
2013-05-25 18:41:38 -0400

    Hopefully last worldgen patch

--- shukaro/enderore/world/OreGen.java
+++ shukaro/enderore/world/OreGen.java
@@ -72,36 +72,56 @@ public class OreGen implements IWorldGenerator
     {
         numberOfBlocks = rand.nextInt(EnderOre.enderOreSize.getInt()) + 3;
         
-        genned = 0;
-        coord.set(x, y, z);
-        list.clear();
+        float f = rand.nextFloat() * (float) Math.PI;
+        double d0 = x + 8 + MathHelper.sin(f) * numberOfBlocks / 8.0F;
+        double d1 = x + 8 - MathHelper.sin(f) * numberOfBlocks / 8.0F;
+        double d2 = z + 8 + MathHelper.cos(f) * numberOfBlocks / 8.0F;
+        double d3 = z + 8 - MathHelper.cos(f) * numberOfBlocks / 8.0F;
+        double d4 = y + rand.nextInt(3) - 2;
+        double d5 = y + rand.nextInt(3) - 2;
         
-        while (genned < numberOfBlocks)
+        for (int l = 0; l <= numberOfBlocks; ++l)
         {
-            if (!(Block.blocksList[world.getBlockId(coord.x, coord.y, coord.z)] == null) && world.getBlockId(coord.x, coord.y, coord.z) == Block.stone.blockID)
+            double d6 = d0 + (d1 - d0) * l / numberOfBlocks;
+            double d7 = d4 + (d5 - d4) * l / numberOfBlocks;
+            double d8 = d2 + (d3 - d2) * l / numberOfBlocks;
+            double d9 = rand.nextDouble() * numberOfBlocks / 16.0D;
+            double d10 = (MathHelper.sin(l * (float) Math.PI / numberOfBlocks) + 1.0F) * d9 + 1.0D;
+            double d11 = (MathHelper.sin(l * (float) Math.PI / numberOfBlocks) + 1.0F) * d9 + 1.0D;
+            int i1 = (int)(d6 - d10 / 2.0D);
+            int j1 = (int)(d7 - d11 / 2.0D);
+            int k1 = (int)(d8 - d10 / 2.0D);
+            int l1 = (int)(d6 + d10 / 2.0D);
+            int i2 = (int)(d7 + d11 / 2.0D);
+            int j2 = (int)(d8 + d10 / 2.0D);
+
+            for (int k2 = i1; k2 <= l1; ++k2)
             {
-                world.setBlock(coord.x, coord.y, coord.z, EnderOre.blockEnderOre.blockID, 0, 2);
-                genned++;
+                double d12 = (k2 + 0.5D - d6) / (d10 / 2.0D);
+                if (d12 * d12 < 1.0D)
+                {
+                    for (int l2 = j1; l2 <= i2; ++l2)
+                    {
+                        double d13 = (l2 + 0.5D - d7) / (d11 / 2.0D);
+                        if (d12 * d12 + d13 * d13 < 1.0D)
+                        {
+                            for (int i3 = k1; i3 <= j2; ++i3)
+                            {
+                                double d14 = (i3 + 0.5D - d8) / (d10 / 2.0D);
+                                Block block = Block.blocksList[world.getBlockId(k2, l2, i3)];
+                                if (block != null)
+                                {
+                                    if (d12 * d12 + d13 * d13 + d14 * d14 < 1.0D && block.blockID == Block.stone.blockID)
+                                    {
+                                        world.setBlock(k2, l2, i3, EnderOre.blockEnderOre.blockID, 0, 1);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
             }
-            
-            if (genned >= numberOfBlocks)
-                return true;
-            
-            for (BlockCoord c : coord.getAdjacent())
-            {
-                if (!(Block.blocksList[world.getBlockId(coord.x, coord.y, coord.z)] == null) && world.getBlockId(c.x, c.y, c.z) == Block.stone.blockID)
-                    list.add(c);
-            }
-            
-            if (list.size() != 0)
-            {
-                coord.set(list.get(rand.nextInt(list.size())));
-                list.remove(coord);
-            }
-            else
-                return true;
         }
-        
         return true;
     }
 }
