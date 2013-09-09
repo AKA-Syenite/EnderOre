a16775c7fab8d69e9e85e93d8b6619851d9ecbe0
Ross Swartz <ross.swartz@gmail.com>
2013-09-09 18:17:56 -0400

    Add tooltips

--- shukaro/enderore/block/EnderDust.java
+++ shukaro/enderore/block/EnderDust.java
@@ -1,10 +1,14 @@
 package shukaro.enderore.block;
 
+import java.util.List;
+
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.Icon;
 
 public class EnderDust extends Item
@@ -29,6 +33,14 @@ public class EnderDust extends Item
     public Icon getIconFromDamage(int par1)
     {
         return itemIcon;
+    }
+    
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void addInformation(ItemStack stack, EntityPlayer player, List infoList, boolean advancedTooltips)
+    {
+    	infoList.add("Powdered essence");
+    	infoList.add("\u00A78" + "\u00A7o" + "Strangely fine...");
     }
 
 }
---
+++ shukaro/enderore/block/ItemBlockEnderOre.java
@@ -0,0 +1,25 @@
+package shukaro.enderore.block;
+
+import java.util.List;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
+
+public class ItemBlockEnderOre extends ItemBlock
+{
+	public ItemBlockEnderOre(int id)
+	{
+		super(id);
+	}
+	
+	@Override
+    @SideOnly(Side.CLIENT)
+    public void addInformation(ItemStack stack, EntityPlayer player, List infoList, boolean advancedTooltips)
+    {
+    	infoList.add("Glowing ore");
+    	infoList.add("\u00A78" + "\u00A7o" + "Exudes an odd aura");
+    }
+}
